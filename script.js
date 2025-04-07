751aa97c0fbdfd32d6a580a73e8730c935050bed
Gehanam <cstewa62@stumail.jccc.edu>
2025-04-06 23:23:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -31,26 +31,17 @@
       { text: 'My blue book value is $400 and use a rubber band for alternater belt!', id: 789012 }
     ];
 
-    // Some and Every Checks
-    // Array.prototype.some() // is at least one person 19?
-    // const isAdult = people.some(function(person) {
-    //   const currentYear = (new Date()).getFullYear();
-    //   if(currentYear - person.year >= 19) {
-    //     return true;
-    //   }
-    // });
+ 
 
     const isClassicCar = car.some(car => ((new Date()).getFullYear()) - car.year >= 35);
 
     console.log({isClassicCar});
-    // Array.prototype.every() // is everyone 19?
+  
 
     const allCars = car.every(car => ((new Date()).getFullYear()) - car.year <= 35);
     console.log({allCars});
 
-    // Array.prototype.find()
-    // Find is like filter, but instead returns just the one you are looking for
-    // find the comment with the ID of 823423
+   
 
 
     const commentFirst = comments.find(comment => comment.id === 111111);
@@ -75,16 +66,4 @@
     console.log(commentNine);
     const commentTen = comments.find(comment => comment.id === 789012);
     console.log(commentTen);
-    // Array.prototype.findIndex()
-    // Find the comment with this ID
-    // delete the comment with the ID of 823423
-    //const index = comments.findIndex(commentNine => comment.id === 123465);
-    //console.log(index);
-
-    // comments.splice(index, 1);
-
-    //const newComments = [
-      //...comments.slice(0, index),
-      //...comments.slice(index + 1)
-    //];
-
+    
